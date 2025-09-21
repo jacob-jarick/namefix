9ce93f9a9abc37b07801acea7b609d4e48b19059
jacob-jarick <mem.namefix@gmail.com>
2025-09-21 22:08:35 +0800

    Update install-modules.sh

--- extra/install-modules.sh
+++ extra/install-modules.sh
@@ -3,6 +3,7 @@
 echo installing modules
 
 
+sudo perl -MCPAN -e "CPAN::Shell->force(qw(install Data::Dumper));"
 sudo perl -MCPAN -e "CPAN::Shell->force(qw(install Tk));"
 sudo perl -MCPAN -e "CPAN::Shell->force(qw(install Tk::JComboBox));"
 sudo perl -MCPAN -e "CPAN::Shell->force(qw(install Tk::DynaTabFrame));"
