713000b1d11ab591eddcd3cddd3e4a44b28aa3bc
jacob-jarick <mem.namefix@gmail.com>
2025-10-17 12:20:07 +0800

    Update config.pm

--- libs/config.pm
+++ libs/config.pm
@@ -190,7 +190,7 @@ else
 &config_init_value('log_stdout',	0, 0, 'bool',	'base'); # CLI: --debug-stdout / GUI: 'Print log to STDOUT'
 &config_init_value('error_stdout',	0, 0, 'bool',	'base'); # # this is set but never used. TODO review and remove
 &config_init_value('error_notify',	0, 0, 'bool',	'base'); # CLI: N/A / GUI: 'Show errors in dialog boxes' (libs/gui/config_dialog.pm)
-&config_init_value('zero_log',		1, 1, 'bool',	'base'); # CLI: TODO add --debug-zero-log / GUI: 'Zero logfile on start'
+&config_init_value('zero_log',		1, 1, 'bool',	'base'); # CLI: --debug-zero-log / GUI: 'Zero logfile on start'
 
 # ---------------------------------------------------------------------------------------------------------------
 # reset_config
@@ -473,6 +473,8 @@ sub set_value
 # --------------------------------------------------------------------------------------------------------------
 # config_init_value
 # --------------------------------------------------------------------------------------------------------------
+
+# sets up $hash entries which represent config options
 
 # deliberately no checks. used only by this library to reduce code duplication
 
