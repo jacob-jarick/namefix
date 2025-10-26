157dc242e95d6e1bf50715f41c8ac4d6396f6db5
Jacob Jarick <mem.namefix@gmail.com>
2025-10-26 20:26:54 +0800

    module installs updated, 4.1.29 build

--- extra/modules_linux.sh
+++ extra/modules_linux.sh
@@ -1,53 +1,62 @@
 #!/bin/bash
 
-# linux_install_modules.sh - Install apt packages needed for building Linux executables
+echo installing apt packages
+
 sudo apt update
-
-sudo apt -y install libperl-dev libx11-dev 
-sudo apt -y install build-essential
-
-
-# sudo apt -y install perl-tk
-
+sudo apt -y install libperl-dev libx11-dev tree
 sudo apt -y install build-essential libpng-dev zlib1g-dev libx11-dev libxt-dev tcl-dev tk-dev
 
-echo install cpanminus if not already installed
+echo cpan
+sudo cpan -u
 sudo cpan App::cpanminus
-
-echo Install PAR::Packer for creating executables
+sudo cpanm CPAN::DistnameInfo
 sudo cpanm PAR::Packer
+sudo cpanm Log::Log4perl
 
-echo installing modules for namefix
+echo debugging modules
 
 sudo cpanm Data::Dumper
 sudo cpanm Data::Dumper::Concise
+sudo cpanm Carp
+
+echo file tag modules
+
 sudo cpanm MP3::Tag
+sudo cpanm -v Image::ExifTool
+
+echo file system modules
+
 sudo cpanm File::Spec::Functions
-sudo cpanm File::Spec::Functions
+sudo cpanm File::Find
+sudo cpanm File::Basename
+sudo cpanm File::Copy
+
+echo Tk modules
 
 sudo cpanm -v --force Tk
-sudo cpanm Tk::JPEG
-sudo cpanm Tk::FontDialog
-sudo cpanm Tk::ColourChooser
-sudo cpanm Config::IniHash
-sudo cpanm MP3::Tag
-sudo cpanm Tk::DirTree
+
 sudo cpanm Tk::Balloon
-sudo cpanm Tk::NoteBook
+sudo cpanm Tk::ColourChooser
+sudo cpanm Tk::DirTree
+sudo cpanm Tk::DynaTabFrame
+sudo cpanm Tk::Event
+sudo cpanm Tk::FontDialog
 sudo cpanm Tk::HList
+sudo cpanm Tk::JComboBox
+sudo cpanm Tk::JPEG
+sudo cpanm Tk::Menu
+sudo cpanm Tk::NoteBook
+sudo cpanm Tk::ProgressBar
 sudo cpanm Tk::Radiobutton
+sudo cpanm Tk::ROText
 sudo cpanm Tk::Spinbox
 sudo cpanm Tk::Text
-sudo cpanm Tk::ROText
-sudo cpanm Tk::DynaTabFrame
-sudo cpanm Tk::Menu
-sudo cpanm Tk::ProgressBar
 sudo cpanm Tk::Text::SuperText
-sudo cpanm Tk::JComboBox
 sudo cpanm Tk::Widget
 sudo cpanm Tk::Wm
-sudo cpanm Tk::Event
+
+echo misc modules
+sudo cpanm Config::IniHash
 sudo cpanm Time/localtime.pm
 
-sudo cpanm -v Image::ExifTool
 
--- extra/modules_windows.bat
+++ extra/modules_windows.bat
@@ -1,16 +1,17 @@
 echo installing modules
 
-rem cpan update
+echo cpan and related tools
 cmd /c cpan -u
 
-rem install cpanminus if not already installed
 cmd /c cpan App::cpanminus
-rem Install PAR::Packer for creating executables 
+cmd /c cpanm CPAN::DistnameInfo
+cmd /c cpanm Log::Log4perl
+
 cmd /c cpanm --notest PAR::Packer
 
-rem  install all Tk modules
-@REM cmd /c cpanm -v Tk
+echo install all Tk modules
 
+@REM cmd /c cpanm -v Tk
 rem  install patched Tk module to avoid build issues
 rem https://github.com/StrawberryPerl/Perl-Dist-Strawberry/issues/87#issuecomment-2292839449
 rem known issues with building Tk on Strawberry Perl 5.32 and later
@@ -18,47 +19,45 @@ rem https://github.com/eserte/perl-tk/issues/87
 rem alternative solution is to install an older Strawberry Perl version (strawberry-perl-5.14.4.1-64bit.msi works)
 cmd /c cpanm --force --notest https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/patched_cpan_modules/Tk-804.036_001.tar.gz
 
-cmd /c cpanm -v Tk::Menu
-cmd /c cpanm -v Tk::DynaTabFrame
-cmd /c cpanm -v Tk::NoteBook
-
-cmd /c cpanm -v Tk::Radiobutton
-cmd /c cpanm -v Tk::Spinbox
-cmd /c cpanm -v Tk::JComboBox
-cmd /c cpanm -v Tk::HList
-cmd /c cpanm -v Tk::DirTree
-
-cmd /c cpanm -v Tk::JPEG
 cmd /c cpanm -v Tk::Balloon
-cmd /c cpanm -v Tk::ProgressBar
 cmd /c cpanm -v Tk::ColourChooser
-
-cmd /c cpanm -v Tk::Text
-cmd /c cpanm -v Tk::ROText
-cmd /c cpanm -v Tk::Text::SuperText
-
+cmd /c cpanm -v Tk::DirTree
+cmd /c cpanm -v Tk::DynaTabFrame
+cmd /c cpanm -v Tk::Event
 cmd /c cpanm -v Tk::FontDialog
+cmd /c cpanm -v Tk::HList
+cmd /c cpanm -v Tk::JComboBox
+cmd /c cpanm -v Tk::JPEG
+cmd /c cpanm -v Tk::Menu
+cmd /c cpanm -v Tk::NoteBook
+cmd /c cpanm -v Tk::ProgressBar
+cmd /c cpanm -v Tk::Radiobutton
+cmd /c cpanm -v Tk::ROText
+cmd /c cpanm -v Tk::Spinbox
+cmd /c cpanm -v Tk::Text
+cmd /c cpanm -v Tk::Text::SuperText
+cmd /c cpanm -v Tk::Widget
+cmd /c cpanm -v Tk::Wm
 
-
-cmd /c cpanm -v --force use Config::IniHash
-
-rem  file tag modules
+echo install file tag modules
 cmd /c cpanm -v MP3::Tag
+cmd /c cpanm -v Image::ExifTool
 
-rem  file modules
+echo install file modules
 cmd /c cpanm -v File::Find
 cmd /c cpanm -v File::Basename
 cmd /c cpanm -v File::Copy
+cmd /c cpanm -v File::Spec::Functions
 
-rem  time
+echo install time module
 cmd /c cpanm -v Time::localtime
 
-rem  debugging
+echo install debugging
 cmd /c cpanm -v Data::Dumper::Concise
+cmd /c cpanm -v Data::Dumper
 cmd /c cpanm -v Carp
 
-rem Image::ExifTool for metadata reading/writing
-cmd /c cpanm -v Image::ExifTool
-
+echo install misc modules
+cmd /c cpanm -v --force use Config::IniHash
 
 
